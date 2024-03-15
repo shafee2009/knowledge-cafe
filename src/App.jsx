21278e2d5632b6349d43c390ca65a00060018f64
s.M Saifan Shafee <shafeenoob4151@gmail.com>
2024-03-15 12:33:21 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Header from './components/header/Header'
+import Blogs from './components/Blogs/blogs'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
